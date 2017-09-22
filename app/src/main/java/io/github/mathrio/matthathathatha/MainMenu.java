7140fee30c7c64245aa0bdae3999f2b50238e8d1
jacobssss14 <jacobssss14@users.noreply.github.com>
2017-09-22 17:13:01 +0800

    Initial App; Many changes to come

---
+++ app/src/main/java/io/github/mathrio/matthathathatha/MainMenu.java
@@ -0,0 +1,30 @@
+package io.github.mathrio.matthathathatha;
+
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    Button bt1, bt2, bt3, bt4 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        bt4 = (Button) findViewById(R.id.button4) ;
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent infoScreen = new Intent (MainMenu.this, About.class) ;
+                startActivity(infoScreen);
+            }
+        });
+    }
+
+
+}
